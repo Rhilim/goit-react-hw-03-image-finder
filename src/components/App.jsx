9340a1d4ed8cf8bd6bc384fc4c49e18f6b1ba6fc
Rhilim <arhilim@gmail.com>
2023-08-17 16:43:53 -0500

    strucrure added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    query: '',
+    images: [],
+    page: 1,
+  };
+
+  changeQuery = () => {};
+
+  render() {
+    return (
+      <>
+        {/* <Searchbar></Searchbar>
+        <ImageGallery>
+          <ImageGalleryItem>
+          <Modal></Modal>
+          </ImageGalleryItem>
+        </ImageGallery>
+        <Button></Button> */}
+      </>
+    );
+  }
+}
